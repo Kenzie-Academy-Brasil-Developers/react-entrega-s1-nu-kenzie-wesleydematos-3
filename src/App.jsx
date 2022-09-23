69d6edc3f1c9dfe8df87ad646b18d8f422a526d0
Wesley de Matos Souza <wesleydematos3@gmail.com>
2022-09-23 11:05:17 -0300

    feat: create no card

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,26 @@ import LandingPage from "./components/LandingPage";
 import Summary from "./components/Summary";
 import Total from "./components/Total";
 import Transactions from "./components/Transactions";
+import carregamento from "./components/img/NoCard.png";
 
 function App() {
   return (
     <div className="App">
+      {/* <LandingPage /> */}
       <Header />
       <main>
         <div className="box_left">
           <Transactions />
-          <Total totalValue={-700} />
+          {/* se houver lancamentos <Total totalValue={-700} /> */}
         </div>
-        <div className="box_right"></div>
-        <Summary />
-        {/* <LandingPage /> */}
+        <div className="box_right">
+          <Summary />
+          <h2>Você ainda não possui nenhum lançamento</h2>
+          <img src={carregamento} alt="Carregando" />
+          <img src={carregamento} alt="Carregando" />
+          <img src={carregamento} alt="Carregando" />
+          {/* se  houver lancamentos */}
+        </div>
       </main>
     </div>
   );
